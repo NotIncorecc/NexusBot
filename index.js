ce0b383a20a337bf4e75d3ae4bf30470e1de83a4
NotIncorecc <85992147+NotIncorecc@users.noreply.github.com>
2024-07-19 21:24:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+require('dotenv').config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//to store commands
+client.commands = new Collection();
+
+const folder_path = path.join(__dirname,'commands');
+const command_types = fs.readdirSync(folder_path);
+
+for (const f of command_types) {
+    const ct_path = path.join(folder_path, f);
+    const command_files = fs.readdirSync(ct_path).filter(n => n.endsWith('.js'));
+    for (const c of command_files) {
+        const c_path = path.join(ct_path, c);
+        const command = require(c_path);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${c_path} is missing a required "data" or "execute" property.`);
+		}
+
+    }
+
+};
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Log in to Discord with your client's token
+client.login(process.env.dtoken);
